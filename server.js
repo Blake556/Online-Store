2c7ef9ff6ea32e1f56e42009101622a91afeaa41
Bkfreedom <88872561+Blake556@users.noreply.github.com>
2023-03-05 11:43:18 -0700

    basic setup

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const PORT = 3009
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.get('/', function(req, res) {
     res.render('index')
+})
+
+app.get('/products', function(req, res) {
+    res.render('products')
+})
+
+app.get('/cart', function(req, res) {
+    res.render('cart')
 })
 
 app.listen(PORT, function() {
